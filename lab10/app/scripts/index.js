c1bfaeb84f67c4d3f15601c74938692805885cdd
Lisa Terwilliger <lct6@augusta.cs.calvin.edu>
2016-11-09 13:41:40 -0500

    Lab10

---
+++ lab10/app/scripts/index.js
@@ -0,0 +1,18 @@
+//Lisa Terwilliger
+//Lab08
+
+//Lab09
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import CommentBox from './commentBox';
+
+import '../css/base.css';
+
+
+
+ReactDOM.render(
+  <CommentBox url="/api/comments" pollInterval={2000} />,
+  document.getElementById('content')
+);
