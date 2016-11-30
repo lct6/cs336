055d5d19aaddada26f31a48f4fa10047126916fc
Lisa Terwilliger <lct6@augusta.cs.calvin.edu>
2016-11-30 13:17:43 -0500

    lab13

---
+++ lab13/app/scripts/index.js
@@ -0,0 +1,28 @@
+//Lisa Terwilliger
+
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+//lab12
+import { Router, Route, Redirect, browserHistory } from 'react-router';
+
+import CommentBox from './commentBox';
+import CommentEdit from './commentEdit';
+
+import '../css/base.css';
+
+//lab13
+import { StoreTools } from './flux';
+StoreTools.startLoadingComments();
+
+
+
+
+ReactDOM.render((
+    <Router history={browserHistory}>
+        <Route path="/" component={CommentBox}/>
+        <Route path="/:id" component={CommentEdit} />
+    </Router>
+), document.getElementById('content')
+);
